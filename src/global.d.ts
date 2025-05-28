2193fe810563c10566f420bcf35c2b490eb8fd1d
thednp <thednp@users.noreply.github.com>
2025-05-28 14:08:37 +0000

    types fix, bump

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,8 @@
 declare module "*.svg?react" {
   import { FC, SVGProps } from "react";
   interface SVGComponentProps extends SVGProps<SVGElement> {
-    width: SVGProps<SVGElement>["width"] | null;
-    height: SVGProps<SVGElement>["height"] | null;
+    width?: SVGProps<SVGElement>["width"] | null;
+    height?: SVGProps<SVGElement>["height"] | null;
   }
   const content: FC<SVGComponentProps>;
   export default content;
